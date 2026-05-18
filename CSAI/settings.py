cbc01ef0f2dafe48cead1d0b32be8ce823c423af
shanusingh24 <pradhyumnasingh2468@gmail.com>
2026-05-19 01:37:53 +0530

    switched from postgre to sqlite

--- CSAI/settings.py
+++ CSAI/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'CSAI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'codesageAi',
-        'USER': 'postgres',
-        'PASSWORD': 'qwerty12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
