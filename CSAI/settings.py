6fffc22ad9cb3199c65689f336cb32fa16d93bbb
shanusingh24 <pradhyumnasingh2468@gmail.com>
2026-05-19 02:33:48 +0530

    nice

--- CSAI/settings.py
+++ CSAI/settings.py
@@ -115,12 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "statics")
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "assets")
+STATIC_URL = 'statics/'
+STATIC_URL = 'statics/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,"statics")]
+STATIC_ROOT=os.path.join(BASE_DIR,"assets")
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
